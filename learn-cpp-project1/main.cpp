d41d66eeb94ecaab2b7b6988ba1caf90874ab792
ribunki <lwh1194@gmail.com>
2025-04-18 17:16:44 +0900

    変数”

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -3,13 +3,15 @@
 
 int main() {
 
-	std::cout << "　　　　 ■\n";
+	/*std::cout << "　　　　 ■\n";
 	std::cout << "　　　 ■　■\n";
 	std::cout << "　　 ■　■　■\n";
 	std::cout << "　  ■　■　■　■\n";
-	std::cout << "■　■　■　■ ■　■\n";
+	std::cout << "■　■　■　■ ■　■\n";*/
+	int a = 100;
 	
-
+	size_t size = sizeof(_int64);
+	std::cout << "サイズ = " << size << "byte\n";
 
 	std::cin.get();
 
