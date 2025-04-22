630b90d9453f394744429139457ecccb94b90b46
ribunki <lwh1194@gmail.com>
2025-04-22 22:35:18 +0900

    kadai

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -1,50 +1,26 @@
-﻿//ピラミッドプログラム
+﻿
 #include <iostream>
+#include <string>
+
 
 int main() {
 
-	/*std::cout << "　　　　 ■\n";
-	std::cout << "　　　 ■　■\n";
-	std::cout << "　　 ■　■　■\n";
-	std::cout << "　  ■　■　■　■\n";
-	std::cout << "■　■　■　■ ■　■\n";*/
-	/*int a = 100;*/
-	
-	/*size_t size = sizeof(_int64);
-	std::cout << "サイズ = " << size << "byte\n";*/
-	/*float x, y, z;
-	x = 100;
-	
 
-	std::cin.get();*/
+	std::string name = "名前：　LI WENHUI";
 
-	//キーボードからの入力
-	//int input = 0;
+	int age = 33;
 
-	//std::cin >> input;
+	std::string shumi = "趣味：　ジョギング　と　ゲーム";
+	std::string sukina_game = "一番好きなゲーム：The Last of Us  ゼルダの伝説  sekiro\n";
 
-	//std::cout << "入力された数値は" << input << "です\n";
 
-	/*float a = 1.24f;
+	std::cout << name << "\n\n";
+	std::cout << "年齢：　" << age << "歳\n\n";
+	std::cout << shumi << "\n\n" << sukina_game << std::endl;
 
-	std::cout << a << std::endl;*/
-	/*float a = 10 % 3;
-	std::cout << a << std::endl;*/
-	char c = 100;
-	int a = 200;
 
-	c = a;
-	std::cout << (int)c << std::endl;
 
-	int num1, num2;
-	
-	std::cout << "整数１を入力してください\n";
-	std::cin >> num1;
 
-	std::cout << "整数2を入力してください\n";
-	std::cin >> num2;
-
-	std::cout << "割り算のけっか" << num1 / num2 << "です\n";
 
 	std::cin.ignore();
 	std::cin.get();
