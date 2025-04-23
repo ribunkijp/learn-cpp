913477ac9624d77e2637feb4e0b59b01a278be16
ribunki <lwh1194@gmail.com>
2025-04-23 11:30:16 +0900

    条件

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -6,19 +6,23 @@
 int main() {
 
 
-	std::string name = "名前：　LI WENHUI";
+	/*std::string name = "名前：　LI WENHUI";
 
-	int age = 33;
-
-	std::string shumi = "趣味：　ジョギング　と　ゲーム";
-	std::string sukina_game = "一番好きなゲーム：The Last of Us  ゼルダの伝説  sekiro\n";
-
-
-	std::cout << name << "\n\n";
-	std::cout << "年齢：　" << age << "歳\n\n";
-	std::cout << shumi << "\n\n" << sukina_game << std::endl;
-
-
+	bool a = 5 > 30;
+	std::cout << a << std::endl;
+	if (a) {
+		std::cout << "真だよ" << std::endl;
+	}
+	else {
+		std::cout << "偽だよ" << std::endl;
+	}*/
+	int a;
+	std::cin >> a;
+	a = a % 3;
+	std::cout << a << std::endl;
+	if (!(a % 3)) {
+		std::cout << "true" << std::endl;
+	}
 
 
 
