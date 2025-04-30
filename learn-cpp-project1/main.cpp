c12d754f2af2edc97f57dda970c9fe0e0eccd251
ribunki <lwh1194@gmail.com>
2025-04-30 17:35:54 +0900

    c++

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -3,82 +3,78 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cctype>
+
+static bool isInteger(const std::string& s) {
+	size_t i = 0;
+	if (s[0] == '-' || s[0] == '+') i = 1;
+	if (i == s.size()) return false;
+
+	for (; i < s.size(); ++i) {
+		if (!std::isdigit(s[i])) return false;
+	}
+	return true;
+}
+static bool isDecimal(const std::string& s) {
+	size_t i = 0;
+	bool pointSeen = false;
+
+	if (s[0] == '-' || s[0] == '+') i = 1;
+	if (i == s.size()) return false;
+
+	for (; i < s.size(); ++i) {
+		if (s[i] == '.') {
+			if (pointSeen) return false;
+			pointSeen = true;
+		}
+		else if (!std::isdigit(s[i])) {
+			return false;
+		}
+	}
+	return pointSeen;
+}
 
 
 int main() {
 
+	std::string firNUM;
+	std::string secNum;
 
-	/*std::string name = "名前：　LI WENHUI";
+	int num1, num2;
 
-	bool a = 5 > 30;
-	std::cout << a << std::endl;
-	if (a) {
-		std::cout << "真だよ" << std::endl;
+	while (true) {
+
+		std::cout << "一つ目の整数を入力してください" << std::endl;
+		std::cin >> firNUM;
+		std::cout << "二つ目の整数を入力してください" << std::endl;
+		std::cin >> secNum;
+
+		if (isInteger(firNUM) && isInteger(secNum)) {
+			num1 = std::stoi(firNUM);
+			num2 = std::stoi(secNum);
+			if (num2 == 0) {
+				std::cout << "0割り算は計算不可です " << std::endl;
+			}
+			else {
+				std::cout << num1 << "+" << num2 << "=" << (num1 + num2) << "\n" << std::endl;
+				std::cout << num1 << "-" << num2 << "=" << (num1 - num2) << "\n" << std::endl;
+				std::cout << num1 << "*" << num2 << "=" << (num1 * num2) << "\n" << std::endl;
+				std::cout << num1 << "/" << num2 << "=" << (num1 / num2) << "\n" << std::endl;
+				std::cout << num1 << "%" << num2 << "=" << (num1 % num2) << "\n" << std::endl;
+				
+				break;
+			}
+		}
+		else if ((isDecimal(firNUM) && isDecimal(secNum)) || (isDecimal(firNUM) && isInteger(secNum)) || (isDecimal(secNum) && isInteger(firNUM))) {
+			std::cout << "整数だけを入力してください。小数は入力しないでください" << std::endl;
+		}
+		else {
+			std::cout << "これは数値ではありません。数値だけを入力してください" << std::endl;
+		}
+
+		continue;
 	}
-	else {
-		std::cout << "偽だよ" << std::endl;
-	}*/
-	/*int a;
-	std::cin >> a;
-	a = a % 3;
-	std::cout << a << std::endl;
-	if (!(a % 3)) {
-		std::cout << "true" << std::endl;
-	}*/
-	/*char res;
-	std::cin >> res;
-
-	if (!(res % 2)) {
-		std::cout << res << "は偶数です" << std::endl;
-	}
-	else {
-		std::cout << res << "は奇数ですよ" << std::endl;
-	}*/
-	/*int a, b ,c;
-
-	std::cout << "一つ目の数を入力してください" << std::endl;
-	std::cin >> a;
-	std::cout << "二つ目の数を入力してください" << std::endl;
-	std::cin >> b;
 	
-	c = a - b;
-
-	if (c == 0) {
-		std::cout << "二つの数は同じです" << std::endl;
-	}
-	else if (c < 0) {
-		std::cout << a << "より" << b << "のほうが大きい値です" << std::endl;
-	}
-	else {
-		std::cout << a << "より" << b << "のほうが小さい値です" << std::endl;
-	}*/
-	//int a, b, c, d, e;
-	//std::vector<std::string>words = //vertor　ベクター
-	//{
-	//	"もっと頑張りましょう",
-	//	"もうすこしがんばりましょう",
-	//	"さらに上を目指しましょう",
-	//	"たいへんよくできました",
-	//	"大変優秀です"
-
-	//};
-	//std::cout << "一つ目の成績を入力してください" << std::endl;
-	//std::cin >> a;
-	//std::cout << "二つ目の成績を入力してください" << std::endl;
-	//std::cin >> b;
-	//std::cout << "三つ目の成績を入力してください" << std::endl;
-	//std::cin >> c;
-	//std::cout << "四つ目の成績を入力してください" << std::endl;
-	//std::cin >> d;
-	//std::cout << "五つ目の成績を入力してください" << std::endl;
-	//std::cin >> e;
-
-	//std::vector<int>arr = { a, b, c, d, e };
-	//std::sort(arr.begin(), arr.end());//std::sort(nums.begin(), nums.end(), std::greater<int>());降序
-	//for (int n = 0; n < arr.size(); n++) {
-	//	std::cout << arr[n] << " " << words[n] << "\n" << std::endl;
-	//}
-	//
 	
 
 
