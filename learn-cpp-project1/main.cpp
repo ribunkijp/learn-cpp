ca5db099bf5766ee7377e1fd97ae8953c928518c
ribunki <lwh1194@gmail.com>
2025-05-13 10:56:05 +0900

    関数

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -22,57 +22,64 @@ static bool isInteger(const std::string& s) {
 	return true;
 }
 
+int Add(int a, int b) {
+	
+	return a + b;
+}
+
 int main() {
 
-	//title
-	std::cout << "数当てゲームへようこそ!!今回の神秘的な数字は１～1000の間の整数です。\n";
-	//乱数生成	
-	srand((unsigned)time(nullptr));
-	int kotae = rand() % 1000 + 1;//answer
-	
-	//ユーザーの入力
-	std::string str;
-	
-	
-	while(true) {
-		
-		std::cin >> str;
-		
-		if (!isInteger(str)) {
-			std::cout << "1～1000の整数を入力してください" << std::endl;
-		}
-		else {
-			int num = std::stoi(str);
-			if (num <= 0 || num > 1000) {
-				std::cout << "1～1000の整数を入力してください" << std::endl;
-				continue;
-			}
-			//ユーザーの入力と答えの差
-			int dist = num - kotae;
-		
-			//判断
-			if (dist == 0) {
-				std::cout << "当たったね、あなたは神様かもね";//当たった
-				break;
-			}
-			else if (dist > 100) {
-				std::cout << "数字が大きすぎました" << std::endl;
-			}
-			else if (dist < -100) {
-				std::cout << "数字が小さすぎました" << std::endl;
-			}
-			else if (dist < 100 && dist > 0) {
-				std::cout << "数字がちょっと大きかった" << std::endl;
-			}
-			else if (dist > -100 && dist < 0) {
-				std::cout << "数字がちょっと小さかった" << std::endl;
-			}
-		}
+	////title
+	//std::cout << "数当てゲームへようこそ!!今回の神秘的な数字は１～1000の間の整数です。\n";
+	////乱数生成	
+	//srand((unsigned)time(nullptr));
+	//int kotae = rand() % 1000 + 1;//answer
+	//
+	////ユーザーの入力
+	//std::string str;
+	//
+	//
+	//while(true) {
+	//	
+	//	std::cin >> str;
+	//	
+	//	if (!isInteger(str)) {
+	//		std::cout << "1～1000の整数を入力してください" << std::endl;
+	//	}
+	//	else {
+	//		int num = std::stoi(str);
+	//		if (num <= 0 || num > 1000) {
+	//			std::cout << "1～1000の整数を入力してください" << std::endl;
+	//			continue;
+	//		}
+	//		//ユーザーの入力と答えの差
+	//		int dist = num - kotae;
+	//	
+	//		//判断
+	//		if (dist == 0) {
+	//			std::cout << "当たったね、あなたは神様かもね";//当たった
+	//			break;
+	//		}
+	//		else if (dist > 100) {
+	//			std::cout << "数字が大きすぎました" << std::endl;
+	//		}
+	//		else if (dist < -100) {
+	//			std::cout << "数字が小さすぎました" << std::endl;
+	//		}
+	//		else if (dist < 100 && dist > 0) {
+	//			std::cout << "数字がちょっと大きかった" << std::endl;
+	//		}
+	//		else if (dist > -100 && dist < 0) {
+	//			std::cout << "数字がちょっと小さかった" << std::endl;
+	//		}
+	//	}
 
-		continue;
-		
-	}
+	//	continue;
+	//	
+	//}
 
+	int c = Add(3, 5);
+	std::cout << c << '\n';
 
 	std::cin.ignore();
 	std::cin.get();
