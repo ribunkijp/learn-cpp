0d4493597d29ad451f27b01b6f0843b4f44f05c6
ribunki <lwh1194@gmail.com>
2025-05-16 19:04:27 +0900

    配列&&ポインター

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -11,43 +11,44 @@
 
 using namespace std;
 
-bool isUruu(int year);
-static string get_new_year_weekday(int year);
+int d = 0;
+double c = 0.0;
 
-int get_month_days(int year, int month);
 
-string get_year_month_weekday(int year, int month);
 
 int main() {
-	/*int input;
-
-	string result;
 	
-	cout << "年を入力\n";
-
-	cin >> input;
 	
-	result = get_new_year_weekday(input);
+	int h[5] = { 10, 11, 12, 13, 14 };
 
-	cout << result << '\n';*/
+	int* p = &h[0];
+	cout << *p << '\n';
+	p--;
 
-	int input_year, input_month;
-
-	string result;
-
-	cout << "年を入力\n";
-
-	cin >> input_year;
+	//p[4] = 200;
 	
-	cout << "月を入力\n";
+	cout << *p << '\n';
 
-	cin >> input_month;
+	/*string kamoku[5] = {
+		"算数",
+		"国語",
+		"英語",
+		"社会",
+		"理科"
 
-	result = get_year_month_weekday(input_year, input_month);
+	};
+	int total = 0;
 
-	cout << result << '\n';
-
+	for (int i = 0; i < 5; i++) {
+		cout << kamoku[i] << "の成績を入力してください\n";
+		int input = 0;
+		cin >> input;
+		total += input;
+	}
 	
+	cout << total / 5 << '\n';*/
+
+
 
 	std::cin.ignore();
 	std::cin.get();
@@ -55,54 +56,3 @@ int main() {
 	return 0;
 }
 
-bool isUruu(int year) {
-
-	return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
-
-}
-
-static string get_new_year_weekday(int year) {
-	const array<string, 7> weekdays  = { "日曜日","月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日" };
-	int days = 0;
-	for (int i = 1900; i < year; i++) {
-		if (isUruu(i)) {
-			days += 366;
-		}
-		else {
-			days += 365;
-		}
-		
-	}
-	return weekdays[(days+1) % 7];
-
-}
-int get_month_days(int year, int month) {
-	if (month == 2) {
-		return isUruu(year) ? 29 : 28;
-	}
-	else if (month == 4 || month == 6 || month == 9 || month == 11) {
-		return 30;
-	}
-	return 31;
-}
-
-string get_year_month_weekday(int year, int month) {
-	const array<string, 7> weekdays = { "日曜日","月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日" };
-	int days = 0;
-	for (int i = 1900; i < year; i++) {
-		if (isUruu(i)) {
-			days += 366;
-		}
-		else {
-			days += 365;
-		}
-
-	}
-	for (int i = 1; i < month; i++) {
-		days += get_month_days(year, i);
-	}
-	
-	
-
-	return weekdays[(days + 1) % 7];
-}
