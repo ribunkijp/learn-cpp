3622ba2b8b51ce70698f446db911b28a68633f25
ribunki <lwh1194@gmail.com>
2025-06-04 09:17:52 +0900

    file I/O

--- learn-cpp-project1/main.cpp
+++ learn-cpp-project1/main.cpp
@@ -5,7 +5,7 @@
 																				
 
 																					Author: ribunki
-																					Date:   2025/05/09
+																					Date:   2025/06/04
 																		
 ==========================================================================================================*/
 
@@ -16,39 +16,97 @@
 #include <random>
 #include <chrono>
 
+
+
 struct Per {
 	unsigned int attack_power{};
 	unsigned int hp{};
 	unsigned int mp{};
 	std::string name{};
 };
-static void read_from_stream(std::vector<Per> pers) {
-	std::ifstream inFile("pers_data.bin", std::ios::in | std::ios::binary);
-	if (!inFile.is_open()) {
+std::ostream& operator<<(std::ostream& os, const Per& p) {
+	os << "Name: " << p.name << "\n"
+		<< "Attack Power: " << p.attack_power << "\n"
+		<< "HP: " << p.hp << "\n"
+		<< "MP: " << p.mp;
+	return os;
+}
+static bool read_from_stream(std::vector<Per> &pers) {
+	
+	std::ifstream in_file("pers_data.bin", std::ios::in | std::ios::binary);
+	if (!in_file.is_open()) {
 		std::cerr << "Error opening file for binary reading!" << std::endl;
-		//return 1;
+		return 1;
 	}
-	//string
 
+	while (true) {
+		Per per;
+		//name
+		int len;
+		in_file.read(reinterpret_cast<char*>(&len), sizeof(len));
+		if (in_file.eof()) break;
+		if (!in_file) { 
+			std::cerr << "Debug: Failed to read name_len or EOF." << std::endl;
+			return false;
+		}
+		if (len > 0) {
+			per.name.resize(len);
+		
+			in_file.read(&per.name[0], len);
+			if (!in_file) { 
+			
+				return false;
+			}
+		}
+		else {
+			per.name.clear();
+		}
+		//attack_power
+		in_file.read(reinterpret_cast<char*>(&per.attack_power), sizeof(per.attack_power));
+		if (!in_file) { 
+			std::cerr << "Debug: Failed to read attack_power or EOF." << std::endl;
+			return false;
+		}
+		//hp
+		in_file.read(reinterpret_cast<char*>(&per.hp), sizeof(per.hp));
+		if (!in_file) {
+			std::cerr << "Debug: Failed to read hp or EOF." << std::endl;
+			return false;
+		}
+		//mp
+		in_file.read(reinterpret_cast<char*>(&per.mp), sizeof(per.mp));
+		if (!in_file) {
+			std::cerr << "Debug: Failed to read mp or EOF." << std::endl;
+			return false;
+		}
+
+		pers.push_back(per);
+	}
+
+	in_file.close();
+
+	return true;
 }
 
-static void write_to_stream(Per per) {
-	std::ofstream outFile("pers_data.bin", std::ios::out | std::ios::binary | std::ios::app);//std::ios::app追加append
-	if (!outFile.is_open()) {
+static bool write_to_stream(Per per) {
+	std::ofstream out_file("pers_data.bin", std::ios::out | std::ios::binary | std::ios::app);//std::ios::app追加append
+	if (!out_file.is_open()) {
 		std::cerr << "Error opening file for binary writing!" << std::endl;
-		//return 1;
+		return false;
 	}
 	
-	//string
+	//name
 	int len = per.name.length();
-	outFile.write(reinterpret_cast<const char*>(&len), sizeof(len));
-	outFile.write(per.name.c_str(), len);
+	out_file.write(reinterpret_cast<const char*>(&len), sizeof(len));
+	out_file.write(per.name.c_str(), len);
 	//attack_power hp mp
-	outFile.write(reinterpret_cast<const char*>(&per.attack_power), sizeof(per.attack_power));
-	outFile.write(reinterpret_cast<const char*>(&per.hp), sizeof(per.hp));
-	outFile.write(reinterpret_cast<const char*>(&per.mp), sizeof(per.mp));
+	out_file.write(reinterpret_cast<const char*>(&per.attack_power), sizeof(per.attack_power));
+	out_file.write(reinterpret_cast<const char*>(&per.hp), sizeof(per.hp));
+	out_file.write(reinterpret_cast<const char*>(&per.mp), sizeof(per.mp));
 
-	outFile.close();
+	out_file.close();
+
+	return true;
 }
 
 Per get_property(std::string& str) {
@@ -70,18 +128,26 @@ int main()
 {
 	std::vector<Per> pers{};
 	
-	read_from_stream(pers);
 
 	std::string input_str{};
 	std::getline(std::cin, input_str);
 
-	//write_to_stream(get_property(input_str));
 	
+	Per new_per = get_property(input_str);
+	
+	write_to_stream(new_per);
+	
+	read_from_stream(pers);
+	
+	for (const auto& p : pers) {
+		std::cout << p << "\n------------------------------\n";
+	}
 
 	
 	
-	return 0;
+
 	
+	return 0;
 	
 }
 
