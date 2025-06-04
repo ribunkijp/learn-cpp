e00d986b249549ef09efba9bbb3d87f5e519ec84
ribunki <lwh1194@gmail.com>
2025-06-05 00:44:23 +0900

    path変更

--- learn_cpp_1/Random.h
+++ learn_cpp_1/Random.h
@@ -6,7 +6,7 @@
 
 namespace Random
 {
-	
+
 	inline std::mt19937 generate()
 	{
 		std::random_device rd{};
@@ -18,23 +18,23 @@ namespace Random
 		return std::mt19937{ ss };
 	}
 
-	
+
 	inline std::mt19937 mt{ generate() };
 
-	
+
 	inline int get(int min, int max)
 	{
 		return std::uniform_int_distribution{ min, max }(mt);
 	}
 
-	
+
 	template <typename T>
 	T get(T min, T max)
 	{
 		return std::uniform_int_distribution<T>{min, max}(mt);
 	}
 
-	
+
 	template <typename R, typename S, typename T>
 	R get(S min, T max)
 	{
--- learn_cpp_1/main.cpp
+++ learn_cpp_1/main.cpp
@@ -1,13 +1,13 @@
-﻿/*========================================================================================================
+/*===================================================================================================
 
-		main[main.cpp]
+	main.cpp
 
-																				
+																			Author：ribunki
+																			Date  ：2025/6/4
 
-																					Author: ribunki
-																					Date:   2025/06/04
-																		
-==========================================================================================================*/
+----------------------------------------------------------------------------------------------------
+
+===================================================================================================*/
 
 #include <iostream>
 #include <string>
@@ -31,8 +31,8 @@ std::ostream& operator<<(std::ostream& os, const Per& p) {
 		<< "MP: " << p.mp;
 	return os;
 }
-static bool read_from_stream(std::vector<Per> &pers) {
-	
+static bool read_from_stream(std::vector<Per>& pers) {
+
 	std::ifstream in_file("pers_data.bin", std::ios::in | std::ios::binary);
 	if (!in_file.is_open()) {
 		std::cerr << "Error opening file for binary reading!" << std::endl;
@@ -46,27 +46,27 @@ static bool read_from_stream(std::vector<Per> &pers) {
 		in_file.read(reinterpret_cast<char*>(&len), sizeof(len));
 		//
 		if (in_file.eof()) break;
-		
+
 		if (!in_file) return false;
 
 		if (len > 0) {
 			per.name.resize(len);
-		
+
 			in_file.read(&per.name[0], len);
 			if (!in_file) return false;
 		}
 		else {
 			per.name.clear();
 		}
-		
+
 		//attack_power
 		in_file.read(reinterpret_cast<char*>(&per.attack_power), sizeof(per.attack_power));
 		if (!in_file) return false;
-		
+
 		//hp
 		in_file.read(reinterpret_cast<char*>(&per.hp), sizeof(per.hp));
 		if (!in_file) return false;
-		
+
 		//mp
 		in_file.read(reinterpret_cast<char*>(&per.mp), sizeof(per.mp));
 		if (!in_file) return false;
@@ -85,7 +85,7 @@ static bool write_to_stream(Per per) {
 		std::cerr << "Error opening file for binary writing!" << std::endl;
 		return false;
 	}
-	
+
 	//name
 	int len = per.name.length();
 	out_file.write(reinterpret_cast<const char*>(&len), sizeof(len));
@@ -102,10 +102,10 @@ static bool write_to_stream(Per per) {
 
 Per get_property(std::string& str) {
 	Per p;
-	std::mt19937 mt{ static_cast<std::mt19937::result_type>(str.size())};
+	std::mt19937 mt{ static_cast<std::mt19937::result_type>(str.size()) };
 
 	std::uniform_int_distribution die8{ 100, 800 };
-	
+
 	p.name = str;
 	p.attack_power = static_cast<unsigned>(die8(mt));
 	p.hp = static_cast<unsigned>(die8(mt));
@@ -118,27 +118,26 @@ Per get_property(std::string& str) {
 int main()
 {
 	std::vector<Per> pers{};
-	
+
 
 	std::string input_str{};
 	std::getline(std::cin, input_str);
 
-	
+
 	Per new_per = get_property(input_str);
-	
+
 	write_to_stream(new_per);
-	
+
 	read_from_stream(pers);
-	
+
 	for (const auto& p : pers) {
 		std::cout << p << "\n------------------------------\n";
 	}
 
-	
-	
 
-	
+
+
+
 	return 0;
-	
-}
 
+}
